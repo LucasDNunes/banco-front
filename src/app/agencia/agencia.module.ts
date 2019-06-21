7c5a3508611a2af6d6ff9b514a542de6190e636b
Lucas <lucasnunes-tb@hotmail.com>
2019-06-20 23:53:54 -0300

    tela de listagem das agencias

--- src/app/agencia/agencia.module.ts
+++ src/app/agencia/agencia.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule, MatButtonModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { AgenciaCadastroComponent } from './agencia-cadastro/agencia-cadastro.component';
 import { AgenciaPesquisaComponent } from './agencia-pesquisa/agencia-pesquisa.component';
+import { AgenciaService } from './agencia.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AgenciaCadastroComponent, AgenciaPesquisaComponent],
+  declarations: [
+    AgenciaCadastroComponent,
+    AgenciaPesquisaComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule
+  ],
+  exports: [
+    AgenciaCadastroComponent,
+    AgenciaPesquisaComponent
+  ],
+  providers: [
+    AgenciaService
   ]
 })
 export class AgenciaModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule, MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material';
+import { AgenciaPesquisaComponent } from './agencia/agencia-pesquisa/agencia-pesquisa.component';
+import { AgenciaCadastroComponent } from './agencia/agencia-cadastro/agencia-cadastro.component';
+import { AgenciaModule } from './agencia/agencia.module';
+
+const rotas: Routes = [
+  { path: 'agencias', component: AgenciaPesquisaComponent },
+  { path: 'agencias/novo', component: AgenciaCadastroComponent },
+  // { path: 'categorias/:id', component: CategoriasCadastroComponent },
+  // { path: 'estados', component: EstadosPesquisaComponent },
+  // { path: 'estados/novo', component: EstadosCadastroComponent },
+  // { path: 'estados/:id', component: EstadosCadastroComponent },
+  // { path: 'cidades', component: CidadesPesquisaComponent },
+  // { path: 'cidades/novo', component: CidadesCadastroComponent },
+  // { path: 'cidades/:id', component: CidadesCadastroComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +28,10 @@ import {MatToolbarModule} from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule.forRoot(rotas),
+    AgenciaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
